41381322d5327ce2ca4bebec442841faaf14862a
Parth Vijay <parth@securitytroops.com>
2020-02-21 11:08:39 +0530

    Last commit

--- test.py
+++ test.py
@@ -66,6 +66,6 @@ else:
         subprocess.getoutput("sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose")
     
 
-print("Launching containers...")
-subprocess.getoutput("sudo /usr/local/bin/docker-compose build")
-subprocess.getoutput("sudo /usr/local/bin/docker-compose up -d")
+# print("Launching containers...")
+# subprocess.getoutput("sudo /usr/local/bin/docker-compose build")
+# subprocess.getoutput("sudo /usr/local/bin/docker-compose up -d")
